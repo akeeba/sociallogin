51d72f6def3b4479d3ecf5c63fc713cb22e22fc5
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-01-03 10:52:15 +0200

    Copyright dates update. Happy New Year 2018!

--- plugins/system/sociallogin/fields/abstraction.php
+++ plugins/system/sociallogin/fields/abstraction.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   AkeebaLoginGuard
- * @copyright Copyright (c)2016-2017 Akeeba Ltd
+ * @copyright Copyright (c)2016-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU General Public License version 3, or later
  */
 
