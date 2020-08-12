9749d7af4e42dce1630f46e2193de41cf0b093c2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-08-12 22:08:09 +0300

    Implement Login with Apple gh-49
    Button styling

--- plugins/sociallogin/apple/apple.php
+++ plugins/sociallogin/apple/apple.php
@@ -74,7 +74,7 @@ class plgSocialloginApple extends AbstractPlugin
 		JLoader::register('plgSocialloginAppleRandomWords', __DIR__ . '/random_words.php');
 
 		// Per-plugin customization
-		$this->buttonImage = 'plg_sociallogin_apple/apple.png';
+		$this->buttonImage = 'plg_sociallogin_apple/apple-white.png';
 	}
 
 	/**
