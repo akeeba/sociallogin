3699fda526e84062c7d8b24d41ce26af004a5d7c
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-03-30 11:15:03 +0300

    # [MEDIUM] Could not install on Joomla 4.1

--- build/templates/script.sociallogin.php
+++ build/templates/script.sociallogin.php
@@ -22,20 +22,6 @@ class Pkg_SocialloginInstallerScript
 	protected $minimumPHPVersion = '7.4.0';
 
 	/**
-	 * The minimum Joomla! version required to install this extension
-	 *
-	 * @var   string
-	 */
-	protected $minimumJoomlaVersion = '3.9.0';
-
-	/**
-	 * The maximum Joomla! version this extension can be installed on
-	 *
-	 * @var   string
-	 */
-	protected $maximumJoomlaVersion = '4.0.999';
-
-	/**
 	 * A list of extensions (modules, plugins) to enable after installation. Each item has four values, in this order:
 	 * type (plugin, module, ...), name (of the extension), client (0=site, 1=admin), group (for plugins).
 	 *
@@ -81,24 +67,6 @@ class Pkg_SocialloginInstallerScript
 		if (!version_compare(PHP_VERSION, $this->minimumPHPVersion, 'ge'))
 		{
 			$msg = "<p>You need PHP $this->minimumPHPVersion or later to install this package</p>";
-			JLog::add($msg, JLog::WARNING, 'jerror');
-
-			return false;
-		}
-
-		// Check the minimum Joomla! version
-		if (!version_compare(JVERSION, $this->minimumJoomlaVersion, 'ge'))
-		{
-			$msg = "<p>You need Joomla! $this->minimumJoomlaVersion or later to install this component</p>";
-			JLog::add($msg, JLog::WARNING, 'jerror');
-
-			return false;
-		}
-
-		// Check the maximum Joomla! version
-		if (!version_compare(JVERSION, $this->maximumJoomlaVersion, 'le'))
-		{
-			$msg = "<p>You need Joomla! $this->maximumJoomlaVersion or earlier to install this component</p>";
 			JLog::add($msg, JLog::WARNING, 'jerror');
 
 			return false;
@@ -178,6 +146,8 @@ class Pkg_SocialloginInstallerScript
 				}
 			}
 		}
+
+		return true;
 	}
 
 	/**
