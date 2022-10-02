6ceeacb7d5e5b16f273ba4715e1c6b706f176f99
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-10-02 20:28:32 +0300

    + Discord integration
    Close #86

---
+++ plugins/sociallogin/discord/src/Integration/OAuth.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package   AkeebaSocialLogin
+ * @copyright Copyright (c)2016-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Joomla\Plugin\Sociallogin\Discord\Integration;
+
+// Protect from unauthorized access
+defined('_JEXEC') || die();
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Http\Http;
+use Joomla\Input\Input;
+use Joomla\Plugin\System\SocialLogin\Library\OAuth\OAuth2Client;
+
+/**
+ * Facebook OAuth client.
+ *
+ * This class is adapted from the Joomla! Framework.
+ */
+class OAuth extends OAuth2Client
+{
+	/**
+	 * @var   array  Options for the OAuth object.
+	 */
+	protected $options;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array           $options      OAuth options array.
+	 * @param   Http            $client       The HTTP client object.
+	 * @param   Input           $input        The input object.
+	 * @param   CMSApplication  $application  The application object.
+	 */
+	public function __construct($options, $client, $input, $application)
+	{
+		$this->options = $options;
+
+		// Setup the authentication and token urls if not already set.
+		if (!isset($this->options['authurl']))
+		{
+			$this->options['authurl'] = 'https://discord.com/api/oauth2/authorize';
+		}
+
+		if (!isset($this->options['tokenurl']))
+		{
+			$this->options['tokenurl'] = 'https://discord.com/api/oauth2/token';
+		}
+
+		if (!isset($this->options['scope']))
+		{
+			$this->options['scope'] = 'identify email';
+		}
+
+		// Call the \Joomla\OAuth2\Client constructor to set up the object.
+		parent::__construct($this->options, $client, $input, $application);
+	}
+
+	/**
+	 * Method to get the current scope
+	 *
+	 * @return  string Comma separated list of permissions.
+	 */
+	public function getScope()
+	{
+		return $this->getOption('scope');
+	}
+
+	/**
+	 * Method used to set permissions.
+	 *
+	 * @param   string  $scope  Comma separated list of permissions.
+	 *
+	 * @return  OAuth  This object for method chaining
+	 */
+	public function setScope($scope)
+	{
+		$this->setOption('scope', $scope);
+
+		return $this;
+	}
+
+}
---
+++ plugins/sociallogin/discord/src/Integration/UserQuery.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @package   AkeebaSocialLogin
+ * @copyright Copyright (c)2016-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Joomla\Plugin\Sociallogin\Discord\Integration;
+
+// Protect from unauthorized access
+defined('_JEXEC') || die();
+
+use Joomla\CMS\Http\Http;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Uri\Uri;
+
+/**
+ * Implements a query to the currently logged-in user through Discord's v10 API.
+ */
+class UserQuery
+{
+	private static $endpoint = 'https://discord.com/api/v10';
+
+	/**
+	 * The HTTP client object to use in sending HTTP requests.
+	 *
+	 * @var    Http
+	 */
+	protected $client;
+
+	/**
+	 * The OAuth token
+	 *
+	 * @var    string
+	 */
+	protected $token;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   Http    $client  The HTTP client object.
+	 * @param   string  $token   The OAuth token.
+	 */
+	public function __construct($client = null, $token = null)
+	{
+		$this->client = $client;
+		$this->token  = $token;
+	}
+
+	/**
+	 * Get the URL for the user's Discord avatar
+	 *
+	 * @param   int  $size  The requested avatar size. Recommended values: 16, 32, 48, 64, 128 and 256.
+	 *
+	 * @return  string
+	 */
+	public function getUserAvatarUrl($size)
+	{
+		$info = $this->getUserInformation();
+
+		if (!isset($info->avatar) || !isset($info->id))
+		{
+			return '';
+		}
+
+		return sprintf("https://cdn.discordapp.com/avatars/%s/%s.png?size=%d", $info->id, $info->avatar, $size);
+	}
+
+	/**
+	 * Get information about the currently logged-in user. The information returned is:
+	 * id             The Discord user ID.
+	 * username       The Discord username, not unique across the platform
+	 * discriminator  The user's 4-digit discord-tag
+	 * avatar         The user's avatar hash
+	 * bot            Whether the user belongs to an OAuth2 application
+	 * system         Whether the user is an Official Discord System user
+	 * mfa_enabled    Whether the user has TFA enabled on their account
+	 * banner         The user's banner hash
+	 * accent_color   The user's banner color encoded as an integer represantation of the hex color code
+	 * locale         The user's language option
+	 * verified       Whether the email on this account has been verified
+	 * email          The user's email
+	 * flags          The flags on a user's account
+	 * premium_type   The type of Nitro subscription on a user's account
+	 * public_flags   The public flags on a user's account
+	 *
+	 * @return  \stdClass  See above.
+	 */
+	public function getUserInformation()
+	{
+		$headers = [
+			'Authorization' => 'Bearer ' . $this->token,
+			//'User-Agent' => sprintf('DiscordBot (%s, %s) AkeebaSocialLogin', Uri::base(false), '4.2.0')
+		];
+
+		$reply = $this->client->get(self::$endpoint . '/users/@me', $headers);
+
+		if ($reply->code > 299)
+		{
+			throw new \RuntimeException("HTTP {$reply->code}: {$reply->body}");
+		}
+
+		return json_decode($reply->body);
+	}
+
+}
