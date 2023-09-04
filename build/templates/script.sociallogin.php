2dc91498f1c2a189a353a257cd8c002c2c9d80fd
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-09-04 22:17:41 +0300

    Changelog update, release notes, and post-installation script update

--- build/templates/script.sociallogin.php
+++ build/templates/script.sociallogin.php
@@ -27,9 +27,11 @@ class Pkg_SocialloginInstallerScript extends InstallerScript
 		['plugin', 'facebook', 1, 'sociallogin'],
 	];
 
-	protected $minimumJoomla = '4.2.0';
-
 	protected $minimumPhp = '7.4.0';
+
+	protected $minimumJoomla = '4.3.0';
+
+	protected $allowDowngrades = true;
 
 	protected $obsoletePlugins = [
 		// Remove Twitter integration, 4.4.0
@@ -495,7 +497,8 @@ class Pkg_SocialloginInstallerScript extends InstallerScript
 			return $extension;
 		}
 
-		function getExtensionsFromManifest(?SimpleXMLElement $xml): array{
+		function getExtensionsFromManifest(?SimpleXMLElement $xml): array
+		{
 			if (empty($xml))
 			{
 				return [];
@@ -515,13 +518,18 @@ class Pkg_SocialloginInstallerScript extends InstallerScript
 		{
 			static $hasOpCache = null;
 
-			if (is_null($hasOpCache)) {
+			if (is_null($hasOpCache))
+			{
 				$hasOpCache = ini_get('opcache.enable')
 				              && function_exists('opcache_invalidate')
-				              && (!ini_get('opcache.restrict_api') || stripos(realpath($_SERVER['SCRIPT_FILENAME']), ini_get('opcache.restrict_api')) === 0);
+				              && (!ini_get('opcache.restrict_api')
+				                  || stripos(
+					                     realpath($_SERVER['SCRIPT_FILENAME']), ini_get('opcache.restrict_api')
+				                     ) === 0);
 			}
 
-			if ($hasOpCache && (strtolower(substr($file, -4)) === '.php')) {
+			if ($hasOpCache && (strtolower(substr($file, -4)) === '.php'))
+			{
 				$ret = opcache_invalidate($file, true);
 
 				@clearstatcache($file);
@@ -542,7 +550,8 @@ class Pkg_SocialloginInstallerScript extends InstallerScript
 			/** @var DirectoryIterator $file */
 			foreach (new DirectoryIterator($path) as $file)
 			{
-				if ($file->isDot() || $file->isLink()) {
+				if ($file->isDot() || $file->isLink())
+				{
 					continue;
 				}
 
@@ -566,26 +575,44 @@ class Pkg_SocialloginInstallerScript extends InstallerScript
 
 		foreach ($extensionsFromPackage as $element)
 		{
-			if (strpos($element, 'plg_') !== 0)
+			$paths = [];
+
+			if (strpos($element, 'plg_') === 0)
+			{
+				[$dummy, $folder, $plugin] = explode('_', $element);
+
+				$paths = [
+					sprintf('%s/%s/%s/services', JPATH_PLUGINS, $folder, $plugin),
+					sprintf('%s/%s/%s/src', JPATH_PLUGINS, $folder, $plugin),
+				];
+			}
+			elseif (strpos($element, 'com_') === 0)
+			{
+				$paths = [
+					sprintf('%s/components/%s/services', JPATH_ADMINISTRATOR, $element),
+					sprintf('%s/components/%s/src', JPATH_ADMINISTRATOR, $element),
+					sprintf('%s/components/%s/src', JPATH_SITE, $element),
+					sprintf('%s/components/%s/src', JPATH_API, $element),
+				];
+			}
+			elseif (strpos($element, 'mod_') === 0)
+			{
+				$paths = [
+					sprintf('%s/modules/%s/services', JPATH_ADMINISTRATOR, $element),
+					sprintf('%s/modules/%s/src', JPATH_ADMINISTRATOR, $element),
+					sprintf('%s/modules/%s/services', JPATH_SITE, $element),
+					sprintf('%s/modules/%s/src', JPATH_SITE, $element),
+				];
+			}
+			else
 			{
 				continue;
 			}
 
-			[$dummy, $folder, $plugin] = explode('_', $element);
-
-			recursiveClearCache(
-				sprintf(
-					'%s/%s/%s/services',
-					JPATH_PLUGINS, $folder, $plugin
-				)
-			);
-
-			recursiveClearCache(
-				sprintf(
-					'%s/%s/%s/src',
-					JPATH_PLUGINS, $folder, $plugin
-				)
-			);
+			foreach ($paths as $path)
+			{
+				recursiveClearCache($path);
+			}
 		}
 
 		clearFileInOPCache(JPATH_CACHE . '/autoload_psr4.php');
