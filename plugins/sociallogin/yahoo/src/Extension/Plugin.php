91de5084ae97e9519352d421693cebc6eb735232
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-12-06 20:55:56 +0200

    + Login with Yahoo

---
+++ plugins/sociallogin/yahoo/src/Extension/Plugin.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package   AkeebaSocialLogin
+ * @copyright Copyright (c)2016-2023 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Joomla\Plugin\Sociallogin\Yahoo\Extension;
+
+defined('_JEXEC') || die();
+
+use Exception;
+use Joomla\CMS\Http\HttpFactory;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\Sociallogin\Yahoo\Integration\OAuth as YahooOAuth;
+use Joomla\Plugin\Sociallogin\Yahoo\Integration\UserQuery;
+use Joomla\Plugin\System\SocialLogin\Library\Data\UserData;
+use Joomla\Plugin\System\SocialLogin\Library\Plugin\AbstractPlugin;
+use Joomla\Registry\Registry;
+
+if (!class_exists(AbstractPlugin::class, true))
+{
+	return;
+}
+
+class Plugin extends AbstractPlugin
+{
+
+	public static function getSubscribedEvents(): array
+	{
+		return array_merge(parent::getSubscribedEvents(), ['onAjaxYahoo' => 'onSocialLoginAjax']);
+	}
+
+	public function init(): void
+	{
+		$this->fgColor = '';
+		$this->bgColor = '';
+		parent::init();
+		$this->buttonImage = 'plg_sociallogin_yahoo/yahoo.png';
+	}
+
+	protected function getConnector(): YahooOAuth
+	{
+		if (is_null($this->connector))
+		{
+			$options         = [
+				'clientid'      => $this->appId,
+				'clientsecret'  => $this->appSecret,
+				'redirecturi'   => Uri::base() . 'index.php?option=com_ajax&group=sociallogin&plugin='
+				                   . $this->integrationName . '&format=raw',
+				'requestparams' => ['state' => $this->getApplication()->getSession()->getToken()],
+			];
+			$httpClient      = HttpFactory::getHttp();
+			$this->connector = new YahooOAuth(
+				$options, $httpClient, $this->getApplication()->input, $this->getApplication()
+			);
+			$this->connector->setScope('openid');
+		}
+
+		return $this->connector;
+	}
+
+	protected function getSocialNetworkProfileInformation(object $connector): array
+	{
+		$tokenArray  = $connector->getToken();
+		$options     = new Registry(['userAgent' => 'Akeeba-Social-Login']);
+		$client      = HttpFactory::getHttp($options);
+		$dUserQuery  = new UserQuery($client, $tokenArray['access_token']);
+		$dUserFields = $dUserQuery->getUserInformation();
+
+		return (array) $dUserFields;
+	}
+
+	protected function mapSocialProfileToUserData(array $socialProfile): UserData
+	{
+		$userData           = new UserData();
+		$userData->name     = $socialProfile['name'] ?? '';
+		$userData->id       = $socialProfile['sub'] ?? '';
+		$userData->email    = $socialProfile['email'] ?? '';
+		$userData->verified = $socialProfile['email_verified'] ?? false;
+
+		return $userData;
+	}
+
+}
---
+++ plugins/sociallogin/yahoo/src/Integration/UserQuery.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package   AkeebaSocialLogin
+ * @copyright Copyright (c)2016-2023 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Joomla\Plugin\Sociallogin\Yahoo\Integration;
+
+defined('_JEXEC') || die();
+
+use Joomla\CMS\Http\Http;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Uri\Uri;
+use RuntimeException;
+
+class UserQuery
+{
+
+	private static $endpoint = 'https://api.login.yahoo.com/openid/v1/userinfo';
+
+	protected $client;
+
+	protected $token;
+
+	public function __construct($client = null, $token = null)
+	{
+		$this->client = $client;
+		$this->token  = $token;
+	}
+
+	public function getUserAvatarUrl()
+	{
+		$info = $this->getUserInformation();
+		if (!isset($info->profile_images))
+		{
+			return '';
+		}
+
+		return $info->profile_images->image64;
+	}
+
+	public function getUserInformation()
+	{
+		$headers = ['Authorization' => 'Bearer ' . $this->token];
+		$reply   = $this->client->get(self::$endpoint, $headers);
+		if ($reply->code > 299)
+		{
+			throw new RuntimeException("HTTP {$reply->code}: {$reply->body}");
+		}
+		$response = json_decode($reply->body, true);
+
+		return $response;
+	}
+
+}
