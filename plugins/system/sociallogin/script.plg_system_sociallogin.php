82fa1557b2b012cab311a211420a8e79e6c711c6
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2025-05-06 18:55:55 +0300

    - Removed PayPal integration (defunct).

--- plugins/system/sociallogin/script.plg_system_sociallogin.php
+++ plugins/system/sociallogin/script.plg_system_sociallogin.php
@@ -8,9 +8,14 @@
 // Prevent direct access
 defined('_JEXEC') || die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Installer\Adapter\ComponentAdapter;
+use Joomla\CMS\Installer\Installer;
+use Joomla\CMS\Table\Extension;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Database\ParameterType;
 
 class plgSystemSocialloginInstallerScript
 {
@@ -25,6 +30,10 @@ class plgSystemSocialloginInstallerScript
 		'folders' => [
 			// Version 1.x helpers, now migrated into Library
 		],
+	];
+
+	protected $removeExtensions = [
+		'plg_sociallogin_paypal',
 	];
 
 	/**
@@ -44,11 +53,17 @@ class plgSystemSocialloginInstallerScript
 		// Do not run on uninstall.
 		if ($type === 'uninstall')
 		{
-			return true;
+			return;
 		}
 
 		// Remove obsolete files and folders
 		$this->removeFilesAndFolders($this->removeFiles);
+
+		// Remove obsolete extensions
+		foreach ($this->removeExtensions as $extension)
+		{
+			$this->uninstallExtension($extension);
+		}
 	}
 
 	/**
@@ -89,6 +104,215 @@ class plgSystemSocialloginInstallerScript
 				Folder::delete($f);
 			}
 		}
+	}
+
+	/**
+	 * Uninstall an extension by name.
+	 *
+	 * @param   string  $extension
+	 *
+	 * @return  bool
+	 */
+	private function uninstallExtension(string $extension): bool
+	{
+		// Let's get the extension ID. If it's not there we can't uninstall this extension, right..?
+		$eid = $this->getExtensionId($extension);
+
+		if (empty($eid))
+		{
+			return false;
+		}
+
+		// Extensions must be marked as not belonging to the package before they can be removed
+		$this->removeExtensionPackageLink($eid);
+
+		// Get an Extension table object and Installer object.
+		$row       = new Extension($this->getDatabase());
+		$installer = Installer::getInstance();
+
+		// Load the extension row or fail the uninstallation immediately.
+		try
+		{
+			if (!$row->load($eid))
+			{
+				return false;
+			}
+		}
+		catch (Throwable $e)
+		{
+			// If the database query fails or Joomla experiences an unplanned rapid deconstruction let's bail out.
+			return false;
+		}
+
+		// Can't uninstalled protected extensions
+		if ((int) $row->locked === 1)
+		{
+			return false;
+		}
+
+		// An extension row without a type? What have you done to your database, you MONSTER?!
+		if (empty($row->type))
+		{
+			return false;
+		}
+
+		// Do the actual uninstallation. Try to trap any errors, just in case...
+		try
+		{
+			return $installer->uninstall($row->type, $eid);
+		}
+		catch (Throwable $e)
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Returns the extension ID for a Joomla extension given its name.
+	 *
+	 * This is deliberately public so that custom handlers can use it without having to reimplement it.
+	 *
+	 * @param   string  $extension  The extension name, e.g. `plg_system_example`.
+	 *
+	 * @return  int|null  The extension ID or null if no such extension exists
+	 */
+	public function getExtensionId(string $extension): ?int
+	{
+		if (isset($this->extensionIds[$extension]))
+		{
+			return $this->extensionIds[$extension];
+		}
+
+		$this->extensionIds[$extension] = null;
+
+		$criteria = $this->extensionNameToCriteria($extension);
+
+		if (empty($criteria))
+		{
+			return $this->extensionIds[$extension];
+		}
+
+		$db    = $this->getDatabase();
+		$query = (method_exists($db, 'createQuery') ? $db->createQuery() : $db->getQuery(true))
+			->select($db->quoteName('extension_id'))
+			->from($db->quoteName('#__extensions'));
+
+		foreach ($criteria as $key => $value)
+		{
+			$type = is_numeric($value) ? ParameterType::INTEGER : ParameterType::STRING;
+			$type = is_bool($value) ? ParameterType::BOOLEAN : $type;
+			$type = is_null($value) ? ParameterType::NULL : $type;
+
+			/**
+			 * This is required since $value is passed by reference in bind(). If we do not do this unholy trick the
+			 * $value variable is overwritten in the next foreach() iteration, therefore all criteria values will be
+			 * equal to the last value iterated. Groan...
+			 */
+			$varName    = 'queryParam' . ucfirst($key);
+			${$varName} = $value;
+
+			$query->where($db->qn($key) . ' = :' . $key)
+				->bind(':' . $key, ${$varName}, $type);
+		}
+
+		try
+		{
+			$this->extensionIds[$extension] = (int) $db->setQuery($query)->loadResult();
+		}
+		catch (RuntimeException $e)
+		{
+			return null;
+		}
+
+		return $this->extensionIds[$extension];
+	}
+
+	/**
+	 * Convert a Joomla extension name to `#__extensions` table query criteria.
+	 *
+	 * The following kinds of extensions are supported:
+	 * * `pkg_something` Package type extension
+	 * * `com_something` Component
+	 * * `plg_folder_something` Plugins
+	 * * `mod_something` Site modules
+	 * * `amod_something` Administrator modules. THIS IS CUSTOM.
+	 * * `file_something` File type extension
+	 * * `lib_something` Library type extension
+	 *
+	 * @param   string  $extensionName
+	 *
+	 * @return  string[]
+	 */
+	private function extensionNameToCriteria(string $extensionName): array
+	{
+		$parts = explode('_', $extensionName, 3);
+
+		switch ($parts[0])
+		{
+			case 'pkg':
+				return [
+					'type'    => 'package',
+					'element' => $extensionName,
+				];
+
+			case 'com':
+				return [
+					'type'    => 'component',
+					'element' => $extensionName,
+				];
+
+			case 'plg':
+				return [
+					'type'    => 'plugin',
+					'folder'  => $parts[1],
+					'element' => $parts[2],
+				];
+
+			case 'mod':
+				return [
+					'type'      => 'module',
+					'element'   => $extensionName,
+					'client_id' => 0,
+				];
+
+			// That's how we note admin modules
+			case 'amod':
+				return [
+					'type'      => 'module',
+					'element'   => substr($extensionName, 1),
+					'client_id' => 1,
+				];
+
+			case 'file':
+				return [
+					'type'    => 'file',
+					'element' => $extensionName,
+				];
+
+			case 'lib':
+				return [
+					'type'    => 'library',
+					'element' => $parts[1],
+				];
+		}
+
+		return [];
+	}
+
+	private function removeExtensionPackageLink(int $eid): void
+	{
+		$db    = $this->getDatabase();
+		$query = (method_exists($db, 'createQuery') ? $db->createQuery() : $db->getQuery(true))
+			->update($db->quoteName('#__extensions'))
+			->set($db->quoteName('package_id') . ' = 0')
+			->where($db->quoteName('extension_id') . ' = :eid')
+			->bind(':eid', $eid, ParameterType::INTEGER);
+		$db->setQuery($query)->execute();
+	}
+
+	private function getDatabase()
+	{
+		return Factory::getContainer()->get(DatabaseInterface::class);
 	}
 
 }
