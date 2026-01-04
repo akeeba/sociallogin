cac0548340f794763db344336d0194e96bdfb9de
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2026-01-04 15:40:55 +0200

    Happy New Year!

--- plugins/sociallogin/linkedin/src/Integration/OAuth.php
+++ plugins/sociallogin/linkedin/src/Integration/OAuth.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package   AkeebaSocialLogin
- * @copyright Copyright (c)2016-2025 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license   GNU General Public License version 3, or later
+ *  @package   AkeebaSocialLogin
+ *  @copyright Copyright (c)2016-2026 Nicholas K. Dionysopoulos / Akeeba Ltd
+ *  @license   GNU General Public License version 3, or later
  */
 
 namespace Akeeba\Plugin\Sociallogin\Linkedin\Integration;
--- plugins/system/sociallogin/media/js/token.js
+++ plugins/system/sociallogin/media/js/token.js
@@ -1,3 +1,15 @@
+/**
+ *  @package   AkeebaSocialLogin
+ *  @copyright Copyright (c)2026 Nicholas K. Dionysopoulos / Akeeba Ltd
+ *  @license   GNU General Public License version 3, or later
+ */
+
+/**
+ *  @package   AkeebaSocialLogin
+ *  @copyright Copyright (c)2026 Nicholas K. Dionysopoulos / Akeeba Ltd
+ *  @license   GNU General Public License version 3, or later
+ */
+
 ((document, Joomla) => {
 
   const copyToClipboardFallback = input => {
